4147bb631cabe468d4e3fa62b5f0df0eaa6adbae
David Escorcia Gomez <escorciagomezdavid@gmail.com>
2022-04-11 09:17:36 -0500

    agregando gitignore

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+#-- La siguiente es una path operation
+@app.get("/")
+def home():
+    return {"Hello": "World"}
+
+# -- uvicorn main:app --reload (para correr en terminal)
