42ef7affff76a5dc1967fa79d08d6f934c88eff5
David Escorcia Gomez <escorciagomezdavid@gmail.com>
2022-04-11 09:53:03 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+#                                           Python
+#realizaremos tipado estatico con typing
+from typing import Optional
 
+#libreria Pydantic, clase BaseModel
+from pydantic import BaseModel
+
+#                                           FastAPI
+from fastapi import FastAPI
+from fastapi import Body
 app = FastAPI()
+
+#                                           Models
+class Person(BaseModel):
+    firstName: str
+    lastName: str
+    age: int
+    hairColor: Optional[str] = None
+    isMarried: Optional[bool] = None
+
 
 #-- La siguiente es una path operation
 @app.get("/")
@@ -8,3 +25,8 @@ def home():
     return {"Hello": "World"}
 
 # -- uvicorn main:app --reload (para correr en terminal)
+
+# request and response
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
